bfd2737a3dd90df6cbb9fa4ca265edc03eb10bda
enbraining <me@fodo.dev>
2024-07-12 22:54:12 +0900

    :recycle: 로그 메시지 수정

--- auth-service/src/main/java/lib/quick/authservice/global/handler/CustomExceptionHandler.java
+++ auth-service/src/main/java/lib/quick/authservice/global/handler/CustomExceptionHandler.java
@@ -1,9 +1,7 @@
 package lib.quick.authservice.global.handler;
 
 import lib.quick.authservice.global.exception.HttpException;
-import lib.quick.authservice.global.exception.MemberNotFoundException;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,15 +11,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class CustomExceptionHandler {
 	@ExceptionHandler(HttpException.class)
 	ResponseEntity<ExceptionResponse> httpException(HttpException exception) {
-		log.error("${exception.message} ${exception.statusCode}");
+		log.error("{}, {}", exception.getStatusCode(), exception.getMessage());
 		return ResponseEntity.status(exception.getStatusCode())
-			.body(new ExceptionResponse(exception.getMessage()));
-	}
-
-	@ExceptionHandler(MemberNotFoundException.class)
-	ResponseEntity<ExceptionResponse> memberNotFoundException(MemberNotFoundException exception) {
-		log.error("${exception.message} ${exception.statusCode}");
-		return ResponseEntity.status(HttpStatus.NOT_FOUND)
 			.body(new ExceptionResponse(exception.getMessage()));
 	}
 }
