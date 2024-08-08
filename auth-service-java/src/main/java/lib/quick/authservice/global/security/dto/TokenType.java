bc45fcb84762e233ace208014bea49056316ea37
enbraining <me@fodo.dev>
2024-08-08 21:01:08 +0900

    :recycle: 파일명 변경 및 자바 삭제

--- auth-service-java/src/main/java/lib/quick/authservice/global/security/dto/TokenType.java
+++
@@ -1,5 +0,0 @@
-package lib.quick.authservice.global.security.dto;
-
-public enum TokenType {
-    ACCESS_TOKEN, REFRESH_TOKEN
-}
--- auth-service-java/src/test/java/lib/quick/authservice/AuthServiceApplicationTests.java
+++
@@ -1,7 +0,0 @@
-package lib.quick.authservice;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class AuthServiceApplicationTests { }
